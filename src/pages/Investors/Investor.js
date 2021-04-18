c12a7113267e4c6d306b884acfc498fab29ae2b4
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-04-18 13:46:06 +0800

    add/investor action fab layout

--- src/pages/Investors/Investor.js
+++ src/pages/Investors/Investor.js
@@ -2,10 +2,15 @@ import React, {useState, useEffect} from 'react'
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
 import { DataGrid } from '@material-ui/data-grid';
 import { dataColumns } from './Investor.config'
 import { useInvestorData } from './Investor.hook';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors'
+import Fab from '@material-ui/core/Fab'
+import DeleteIcon from '@material-ui/icons/Delete'
+import AddIcon from '@material-ui/icons/Add'
+import EditIcon from '@material-ui/icons/Edit'
 
 export default function Investor() {
 
@@ -34,6 +39,38 @@ export default function Investor() {
           />
         </CardContent>
       </Card>
+      <div
+        style={{
+          position: 'fixed',
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '1rem',
+          bottom: '5rem',
+          right: '5rem',
+        }}
+      >
+        {
+          selectedInvestor !== -1 && (
+          <>
+            <ThemeProvider
+              theme={createMuiTheme({ palette: { primary: red } })}
+            >
+              <Fab color='primary'>
+                <DeleteIcon />
+              </Fab>
+            </ThemeProvider>
+            <Fab>
+              <EditIcon />
+            </Fab>
+          </>
+          )
+        }
+        <Fab
+          color='primary'
+        >
+          <AddIcon style={{color: '#fff'}} />
+        </Fab>
+      </div>
     </section>
   )
 }
