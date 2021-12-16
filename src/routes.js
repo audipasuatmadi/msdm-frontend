02339114a194179779311e58987ed46fb7846e82
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-12-16 17:41:07 +0800

    update routes

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,12 @@ import DashboardIcon from '@material-ui/icons/Dashboard';
 import LockIcon from '@material-ui/icons/Lock';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
-import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 import Employees from './pages/Employees/Employees';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Departemen from './pages/Departement/Departement';
 import Investor from './pages/Investors/Investor';
 import Stakeholders from './pages/Stakeholders/Stakeholders';
 import Roles from './pages/Roles/Roles';
-import About from './pages/About/About';
 
 const routes = [
   {
@@ -50,13 +48,6 @@ const routes = [
     icon: MonetizationOnIcon,
     route: '/investors',
     component: Investor,
-  },
-  {
-    name: 'Tentang Kami',
-    icon: EmojiEmotionsIcon,
-    divider: true,
-    route: '/about',
-    component: About,
   },
 ];
 
